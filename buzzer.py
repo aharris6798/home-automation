dca87d0d599c13dc2c598cefd0769b6a0c400b66
aharris6798 <aharris@salisbury6c.ac.uk>
2016-04-20 12:57:09 +0100

    Update buzzer.py

--- buzzer.py
+++ buzzer.py
@@ -1,1 +1,21 @@
+import RPi.GPIO as gpio
+import board
+
 class Buzzer:
+    
+    def __init__(self, board, buzzerPin):
+        self.board = board
+        self.buzzerPin = buzzerPin
+        self.buzzMethod()
+    
+    def buzzMethod(self):
+        self.board.setup(buzzerPin, self.board.OUT)
+        self.board.output(buzzerPin, HIGH)
+        time.sleep(2)
+        self.board.output(buzzerPin, LOW)
+
+if __name__ == "__main__":
+    gpio.setmode(gpio.BOARD)
+    buzzerPin = 38
+    board = Board()
+    Buzzer(buzzerPin, board)
