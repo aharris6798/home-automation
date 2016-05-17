c0cfd28beddf7a93490ae1e9fa75f1cecd395442
root <root@adam.pi>
2016-05-17 09:41:53 +0100

    chnages

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-import board from Board
-import led from Led
+from board import Board
+from Led import Led
+from temp2 import Temperature
+from Rgb import RGB_Led
+from LCD1602 import LCD1602
+from time import sleep
+from button import Button
+from buzzer import Buzzer
+from pir import Pir
+
+    while 
 
 rpi = Board()
-led1 = Led(rpi,11 )
+led1 = Led(rpi,11)
 led1.turn_on()
 led1.turn_off()
