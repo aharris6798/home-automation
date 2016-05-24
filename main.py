14ccf231a140b131e7285781b959e76f501c0593
aharris6798 <aharris@salisbury6c.ac.uk>
2016-05-24 09:54:06 +0100

    oii

--- main.py
+++ main.py
@@ -8,9 +8,16 @@ from button import Button
 from buzzer import Buzzer
 from pir import Pir
 
-    while 
 
 rpi = Board()
-led1 = Led(rpi,11)
-led1.turn_on()
-led1.turn_off()
+led = Led(rpi,11)
+rgb = RGB_Led(26, 19, 13)
+button = Button(rpi, 27)
+temp = Temperature()
+lcd = LCD1602(rpi)
+buzzer = Buzzer()
+pir = Pir
+
+while True:
+    if
+lcd.message()
